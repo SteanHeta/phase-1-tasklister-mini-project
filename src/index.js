cf9eff89bae66e5caac33d1e1b20c473baec363e
Stean <stean.ngugi@student.moringaschool.com>
2025-04-03 08:50:22 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent page reload
+
+      const input = document.querySelector("#new-task-description");
+      const task = document.createElement("li");
+      task.textContent = input.value;
+
+      taskList.appendChild(task);
+      input.value = ""; // Clear input field
+  });
 });
+
+
